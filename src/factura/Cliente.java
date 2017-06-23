38ff971878a0ec73d0fb639e085e22381ac3436f
cintymascio <cinty.mascio@gmail.com git config --global user.name cinthia git config --global user.name cinty.mascio git config --global user.name cintymasciogit config --global user.email cinty.mascio@gmail.com>
2017-06-22 21:34:15 -0300

    modifique atributos de cliente agregando aellido y getters and setters

--- src/factura/Cliente.java
+++ src/factura/Cliente.java
@@ -11,14 +11,24 @@ package factura;
  */
 public class Cliente {
     private String nombre;
-
+    private String apellido;
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+        
     }
     
+    
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String nombre) {
+        this.apellido = nombre;
+        
+    }
     
 }
